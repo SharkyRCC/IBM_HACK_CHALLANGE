7103c2610d0706e2500ed41d7a9257a23e723268
Shivam choudhary <33366670+SharkyRCC@users.noreply.github.com>
2018-10-02 14:08:39 +0530

    Update Auth.R

--- Auth.R
+++ Auth.R
@@ -1,14 +1,14 @@
-library(ROAuth)
+ library(ROAuth)
 library(twitteR)
 
-consumer_key <-"I4jNBCEz4PFtD63iFNM3wrFBQ"
-consumer_secret <- "MSKq9nhH7cRx449diLClVVGz8kgvqOEFK5lrQ7UOGs5fzsUbjM"
-access_token<-"2371184034-iqSAeOCugVZU20w4MVDuHLFcD67LFkcjnYlWNCD"
-access_secret <- "tkb5oBKlb8DOjzexoxeDAqUo8XDy6NtEIR81WwumgpHET"
+consumer_key <-"iViqmwMTT3YdyAnIXzDEc4n7l"
+consumer_secret <- "gNN9TYqE6izXPhw402SaQYf6tw1PMBoyz9am4hJJmNPnKy6GR7"
+access_token<-"2371184034-894102rczEyLcopJW1jQmz0ISqkqpLV09TfMtXl"
+access_secret <- "lVL4qig9qRdr8FMrJ9ZTZ12nUM2UnzkdnCWRU2C3CoOjX"
 
  setup_twitter_oauth(consumer_key ,consumer_secret, access_token,  access_secret )
  
-cred <- OAuthFactory$new(consumerKey=consumer_key,consumerSecret=consumer_secret,requestURL='https://api.twitter.com/oauth/request_token',accessURL='https://api.twitter.com/oauth/access_token',authURL='https://api.twitter.com/oauth/authorize')
+cred <- OAuthFactory$new(consumerKey='iViqmwMTT3YdyAnIXzDEc4n7l', consumerSecret='gNN9TYqE6izXPhw402SaQYf6tw1PMBoyz9am4hJJmNPnKy6GR7',requestURL='https://api.twitter.com/oauth/request_token',accessURL='https://api.twitter.com/oauth/access_token',authURL='https://api.twitter.com/oauth/authorize')
 
 cred$handshake(cainfo="cacert.pem")
 #After this you will be redirected to a URL where you click on authorize app and get the passkey to be entered in RStudio
