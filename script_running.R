321c52f9f54bdffb6a29f30a85afbd530569a725
Shivam choudhary <33366670+SharkyRCC@users.noreply.github.com>
2018-10-02 14:08:10 +0530

    Update script_running.R

--- script_running.R
+++ script_running.R
@@ -1,14 +1,14 @@
  library(ROAuth)
 library(twitteR)
 
-consumer_key <-"AKJsxNqX2D8uTo9orgjRirvWL"
-consumer_secret <- "QOKk0ctHhbXNQ5QaipqofrZQzWM92mfkcoP60xe7HJzjSUCz6F"
-access_token<-"2617540074-5l6gGJhCP8iw9DS7sVD9qsFaUGfWGO9fqlHt5Wg"
-access_secret <- "VVMfNIzgPEUmCk5QyIWr5A4ZSC2Lxy7CERoUtWs4jAe0l"
+consumer_key <-"iViqmwMTT3YdyAnIXzDEc4n7l"
+consumer_secret <- "gNN9TYqE6izXPhw402SaQYf6tw1PMBoyz9am4hJJmNPnKy6GR7"
+access_token<-"2371184034-894102rczEyLcopJW1jQmz0ISqkqpLV09TfMtXl"
+access_secret <- "lVL4qig9qRdr8FMrJ9ZTZ12nUM2UnzkdnCWRU2C3CoOjX"
 
  setup_twitter_oauth(consumer_key ,consumer_secret, access_token,  access_secret )
  
-cred <- OAuthFactory$new(consumerKey='AKJsxNqX2D8uTo9orgjRirvWL', consumerSecret='QOKk0ctHhbXNQ5QaipqofrZQzWM92mfkcoP60xe7HJzjSUCz6F',requestURL='https://api.twitter.com/oauth/request_token',accessURL='https://api.twitter.com/oauth/access_token',authURL='https://api.twitter.com/oauth/authorize')
+cred <- OAuthFactory$new(consumerKey='iViqmwMTT3YdyAnIXzDEc4n7l', consumerSecret='gNN9TYqE6izXPhw402SaQYf6tw1PMBoyz9am4hJJmNPnKy6GR7',requestURL='https://api.twitter.com/oauth/request_token',accessURL='https://api.twitter.com/oauth/access_token',authURL='https://api.twitter.com/oauth/authorize')
 
 cred$handshake(cainfo="cacert.pem")
 	
